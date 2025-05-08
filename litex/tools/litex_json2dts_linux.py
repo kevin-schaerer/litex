87d2b62af04f1e68d122865c6e3351f777a352aa
Kevin Schaerer <kevin.schaerer@nettimelogic.com>
2025-05-08 21:28:42 +0200

    fix

--- litex/tools/litex_json2dts_linux.py
+++ litex/tools/litex_json2dts_linux.py
@@ -83,7 +83,7 @@ def generate_dts(d, initrd_start=None, initrd_size=None, initrd=None, root_devic
         chosen {{
             bootargs = "{console} {rootfs}{ip}";""".format(
     console = "console=liteuart earlycon=liteuart,0x{:x}".format(d["csr_bases"]["uart"]),
-    rootfs  = "root=/dev/{}".format(root_device),
+    rootfs  = "root=initramfs",
     ip      = get_eth_ip_config())
 
     if initrd_enabled is True:
